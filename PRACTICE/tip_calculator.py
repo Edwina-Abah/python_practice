fcea944259a71cb11b2db0ae1278d52e9a62fd41
Edwina-Abah <eabah2022@gmail.com>
2022-11-23 13:55:56 +0100

    "Added conclution to the py file"

--- PRACTICE/tip_calculator.py
+++ PRACTICE/tip_calculator.py
@@ -8,3 +8,5 @@ total_bill = bill + total_bill_amount
 bill_per_person = total_bill/people
 final_amount = round(bill_per_person, 2)
 print(f"Each person should pay: ${bill_per_person}")
+
+print("Thank you for your time," "+" "Do visit again.")
